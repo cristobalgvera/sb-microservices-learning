afb721e18f610f597b1d4cf3eb158847f9d221c4
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2022-03-17 12:31:55 -0300

    test(person-data): added context tests

--- person-data-api/src/test/java/com/nttdata/persondataapi/PersonDataApiApplicationTests.java
+++ person-data-api/src/test/java/com/nttdata/persondataapi/PersonDataApiApplicationTests.java
@@ -1,13 +1,20 @@
 package com.nttdata.persondataapi;
 
+import com.nttdata.persondataapi.persondata.PersonDataController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class PersonDataApiApplicationTests {
+    @Autowired
+    private PersonDataController personDataController;
 
     @Test
     void contextLoads() {
+        assertThat(personDataController).isNotNull();
     }
 
 }
