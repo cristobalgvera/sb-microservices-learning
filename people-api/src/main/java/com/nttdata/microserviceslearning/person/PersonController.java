f02935c8b649639fcbfaeadfce3e0b72838635e3
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2022-03-17 11:59:34 -0300

    feat(person): added find by id endpoint

--- people-api/src/main/java/com/nttdata/microserviceslearning/person/PersonController.java
+++ people-api/src/main/java/com/nttdata/microserviceslearning/person/PersonController.java
@@ -6,8 +6,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/v1/people")
@@ -18,5 +21,10 @@ public class PersonController {
     @GetMapping
     public ResponseEntity<Page<PersonDto>> findAll(Pageable pageable) {
         return ResponseEntity.ok(personService.findAll(pageable));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PersonDto> findById(@PathVariable UUID id) {
+        return ResponseEntity.ok(personService.findById(id));
     }
 }
--- people-api/src/main/java/com/nttdata/microserviceslearning/person/PersonService.java
+++ people-api/src/main/java/com/nttdata/microserviceslearning/person/PersonService.java
@@ -2,9 +2,13 @@ package com.nttdata.microserviceslearning.person;
 
 import com.nttdata.microserviceslearning.person.dto.PersonDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -16,5 +20,12 @@ public class PersonService {
         return personRepository
                 .findAll(pageable)
                 .map(personMapper::toDto);
+    }
+
+    public PersonDto findById(UUID id) {
+        var person = personRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Person with id '%s' not found".formatted(id)));
+
+        return personMapper.toDto(person);
     }
 }
