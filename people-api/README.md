99fdc0f5b12d3ba135061574e4072e48369abe72
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2022-03-17 11:13:51 -0300

    chore(api): created base spring boot project

---
+++ people-api/README.md
@@ -0,0 +1,8 @@
+# People Service
+
+JDK: 17.0.2
+
+``` bash
+# In order to run the project run this command
+mvn spring-boot:run
+```
---
+++ people-api/src/main/java/com/nttdata/microserviceslearning/MicroservicesLearningApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.microserviceslearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicesLearningApplication.class, args);
+    }
+
+}
