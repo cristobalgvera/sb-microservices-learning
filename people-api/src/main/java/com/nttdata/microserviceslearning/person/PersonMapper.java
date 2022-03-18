6f8f7f28727ce0ac2b780e4971e752d0980ce0ed
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2022-03-18 10:10:21 -0300

    feat(person): added load balancer value to person dto

--- people-api/src/main/java/com/nttdata/microserviceslearning/person/PersonMapper.java
+++ people-api/src/main/java/com/nttdata/microserviceslearning/person/PersonMapper.java
@@ -2,8 +2,22 @@ package com.nttdata.microserviceslearning.person;
 
 import com.nttdata.microserviceslearning.person.dto.PersonDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+import org.springframework.beans.factory.annotation.Value;
 
 @Mapper(componentModel = "spring")
-public interface PersonMapper {
-    PersonDto toDto(Person person);
+public abstract class PersonMapper {
+    private static final String MAP_BALANCER = "MAP_BALANCER";
+
+    @Value("${config.balancer.test}")
+    private String balancer;
+
+    @Mapping(target = "balancer", source = "balancer", qualifiedByName = MAP_BALANCER)
+    public abstract PersonDto toDto(Person person);
+
+    @Named(MAP_BALANCER)
+    protected String mapBalancer(String entityBalancer) {
+        return balancer;
+    }
 }
--- people-api/src/main/java/com/nttdata/microserviceslearning/person/PersonService.java
+++ people-api/src/main/java/com/nttdata/microserviceslearning/person/PersonService.java
@@ -2,7 +2,6 @@ package com.nttdata.microserviceslearning.person;
 
 import com.nttdata.microserviceslearning.person.dto.PersonDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
